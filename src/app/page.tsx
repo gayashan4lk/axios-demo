baba252a5260410f7ddd5a47bf05bfa7c4e3175f
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-05-05 03:55:12 +0530

    install daisyUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image';
 
 export default function Home() {
-	return <main>Hi</main>;
+	return (
+		<main>
+			<h1>Axios Crash Course</h1>
+			<div>
+				<button className='btn btn-primary'>GET</button>
+			</div>
+		</main>
+	);
 }
